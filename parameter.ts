db9faa4071277dd41f6506bf63482c4c0c3dc02d
ktjn1129 <97653478+ktjn1129@users.noreply.github.com>
2024-03-08 23:24:48 +0900

    parameter

--- parameter.ts
+++ parameter.ts
@@ -1,5 +1,6 @@
 export {};
 
+// 1.
 // オプショナルパラメータ
 let bmi: (height: number, weight: number, printable?: boolean) => number = (
     height: number,
@@ -8,7 +9,7 @@ let bmi: (height: number, weight: number, printable?: boolean) => number = (
 ): number => {
     const bmi: number = weight / (height * height);
 
-    if(printable) {
+    if (printable) {
         console.log({ bmi });
     }
     return bmi;
@@ -16,10 +17,23 @@ let bmi: (height: number, weight: number, printable?: boolean) => number = (
 
 bmi(1.78, 86);
 
-
+// 2.
 // デフォルトパラメータ
 const nextYearSalary = (currentSalary: number, rate: number = 1.1) => {
     return currentSalary * rate;
 };
 
 console.log(nextYearSalary(1000, 1.05));
+
+// 3.
+// Restパラメータ
+const reducer = (accumulator: number, currentValue: number) => {
+    console.log({ accumulator, currentValue });
+    return accumulator + currentValue;
+};
+
+const sum: (...values: number[]) => number = (...values: number[]): number => {
+    return values.reduce(reducer);
+};
+
+console.log(sum(1, 2, 3, 4, 5));
