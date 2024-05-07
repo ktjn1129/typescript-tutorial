4a9bca8381df4d07ac73b983d9aa4229eb009390
ktjn1129 <ktjn1129@gmail.com>
2024-05-07 22:25:47 +0900

    add doc

--- parameter.ts
+++ parameter.ts
@@ -1,39 +1,30 @@
 export {};
 
-// 1.
-// オプショナルパラメータ
+// 1.オプショナルパラメータ
 let bmi: (height: number, weight: number, printable?: boolean) => number = (
-    height: number,
-    weight: number,
-    printable?: boolean
+  height: number,
+  weight: number,
+  printable?: boolean
 ): number => {
-    const bmi: number = weight / (height * height);
+  const bmi: number = weight / (height * height);
 
-    if (printable) {
-        console.log({ bmi });
-    }
-    return bmi;
+  if (printable) {
+    console.log({ bmi });
+  }
+  return bmi;
 };
 
-bmi(1.78, 86);
-
-// 2.
-// デフォルトパラメータ
+// 2.デフォルトパラメータ
 const nextYearSalary = (currentSalary: number, rate: number = 1.1) => {
-    return currentSalary * rate;
+  return currentSalary * rate;
 };
 
-console.log(nextYearSalary(1000, 1.05));
-
-// 3.
-// Restパラメータ
+// 3.Restパラメータ：不特定多数の引数を配列として受け取る構文
 const reducer = (accumulator: number, currentValue: number) => {
-    console.log({ accumulator, currentValue });
-    return accumulator + currentValue;
+  console.log({ accumulator, currentValue });
+  return accumulator + currentValue;
 };
 
 const sum: (...values: number[]) => number = (...values: number[]): number => {
-    return values.reduce(reducer);
+  return values.reduce(reducer);
 };
-
-console.log(sum(1, 2, 3, 4, 5));
