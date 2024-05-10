25f145b810baab074befb8a9c217c0e7cac61c27
ktjn1129 <ktjn1129@gmail.com>
2024-05-10 21:57:08 +0900

    add doc

---
+++ as-const.ts
@@ -0,0 +1,15 @@
+export {};
+
+let name = "Taro";
+name = "Tanaka";
+
+let nickname = "Taroimo" as const;
+// nickname = "Tomato";
+
+let profile = {
+  name: "Taro",
+  height: 177,
+} as const;
+
+// profile.name = "Tanaka";
+// profile.height = 180;
